fa1ef95bc325a38d774a4af7697de00be5716b83
joelcamacho <jcamacho82@hotmail.com>
2016-04-25 20:16:53 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Program Start")
+
+var inventory = [
+{
+	item : "milk",
+	price: 4.25
+},
+{
+	item : "bread",
+	price : 2.75
+},
+{
+	item : "eggs",
+	price : 3.50
+},
+{
+	item : "cereal",
+	price : 4.25
+},
+{
+	item : "rice",
+	price : 8.00
+},
+{
+	item : "bag of apples",
+	price : 5.00
+},
+{
+	item : "crackers",
+	price : 3.60
+},
+{
+	item : "carrots",
+	price : 4.50
+}
+
+];
+function addToInventory(item) {
+	
+}
